63b6b9907c15df69395a76dccd0a111f0bf159c1
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2025-08-27 17:17:44 +0900

    c

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -5,8 +5,15 @@ public class Rotate : MonoBehaviour
     [SerializeField] float rotateSpeed = 1f;
     [SerializeField] float maxRotations = 3;
     float currentRotation;
+
+    float delayToRotate = 2;
     void FixedUpdate()
     {
+        if (delayToRotate > 0)
+        {
+            delayToRotate -= Time.deltaTime;
+            return;
+        }
         if (currentRotation / 360 > maxRotations) return;
         transform.Rotate(Vector3.up, rotateSpeed * Time.fixedDeltaTime);
         currentRotation += rotateSpeed * Time.fixedDeltaTime;
