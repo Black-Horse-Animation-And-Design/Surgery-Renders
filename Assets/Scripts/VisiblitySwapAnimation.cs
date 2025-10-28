8f999b03d959abef20dc44c3435e6a5553cd78ad
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2025-10-29 02:45:50 +0900

    c

--- Assets/Scripts/VisiblitySwapAnimation.cs
+++ Assets/Scripts/VisiblitySwapAnimation.cs
@@ -1,17 +1,16 @@
-using UnityEditor;
 using UnityEngine;
 
 public class VisiblitySwapAnimation : MonoBehaviour
 {
     [SerializeField] GameObject[] objectsToToggle;
-    int index = -1;
+    int index;
 
     [SerializeField] bool stops;
 
     public void ToggleObjects()
     {
-        if (index >= objectsToToggle.Length - 1 && stops) EditorApplication.isPlaying = false;
-        if (index > 0) objectsToToggle[index].SetActive(false);
+
+        objectsToToggle[index].SetActive(false);
         index++;
         objectsToToggle[index].SetActive(true);
     }
