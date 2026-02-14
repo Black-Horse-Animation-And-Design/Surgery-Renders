4b120de7fa6ae5d766f0a74df95d8b99eb041f2d
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2026-02-14 12:16:44 -0700

    c

--- Assets/Scripts/CameraPivot.cs
+++ Assets/Scripts/CameraPivot.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class CameraPivot : MonoBehaviour
 {
+    [SerializeField] float speed = 1;
+    private void Awake()
+    {
+        Time.timeScale = speed;
+    }
+
     public void OnApplicationQuit()
     {
         Application.Quit();
